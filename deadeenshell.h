c324e9339bbff818d1a23f56300c5d6b3ab48274
Deadeen <141264828+Deadeen@users.noreply.github.com>
2023-11-16 01:20:06 +0200

    back

--- deadeenshell.h
+++ deadeenshell.h
@@ -132,10 +132,10 @@ int _putfd(char c, int fd);
 int _putsfd(char *str, int fd);
 
 /* string_functions.c */
-int _stringlen(char *);
-int _strcomp(char *, char *);
-char *start_wth(const char *, const char *);
-char *_stringcat(char *, char *);
+int _strlen(char *);
+int _strcmp(char *, char *);
+char *starts_with(const char *, const char *);
+char *_strcat(char *, char *);
 
 /* string_functions2.c */
 char *_strcpy(char *, char *);
