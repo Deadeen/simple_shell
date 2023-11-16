9201f8b09bf39b82f0a4d7f275db97a285fcc3c3
Deadeen <141264828+Deadeen@users.noreply.github.com>
2023-11-16 03:59:33 +0200

    pasthist

--- builtin_emulators2.c
+++ builtin_emulators2.c
@@ -1,7 +1,7 @@
 #include "deadeenshell.h"
 
 /**
- * _myhistory - displays the history list, one command by line, preceded
+ * _myhistory - displays the pasthist list, one command by line, preceded
  *              with line numbers, starting at 0.
  * @info: Structure containing potential arguments. Used to maintain
  *        constant function prototype.
@@ -9,7 +9,7 @@
  */
 int _myhistory(info_t *info)
 {
-	print_list(info->history);
+	print_list(info->pasthist);
 	return (0);
 }
 
