1d9fc04c296be8975486a84dd46c5e04508b267e
Deadeen <141264828+Deadeen@users.noreply.github.com>
2023-11-16 03:43:13 +0200

    bashshellbuff

--- getline.c
+++ getline.c
@@ -15,7 +15,7 @@ ssize_t input_buf(info_t *info, char **buf, size_t *len)
 
 	if (!*len) /* if nothing left in the buffer, fill it */
 	{
-		/*bfree((void **)info->cmd_buf);*/
+		/*bfree((void **)info->bashshellbuff);*/
 		free(*buf);
 		*buf = NULL;
 		signal(SIGINT, sigintHandler);
@@ -37,7 +37,7 @@ ssize_t input_buf(info_t *info, char **buf, size_t *len)
 			/* if (_strchr(*buf, ';')) is this a command chain? */
 			{
 				*len = r;
-				info->cmd_buf = buf;
+				info->bashshellbuff = buf;
 			}
 		}
 	}
