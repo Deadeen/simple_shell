358c7665f289f3775fb5c8b5ff4b2b09ac920630
Deadeen <141264828+Deadeen@users.noreply.github.com>
2023-11-16 04:53:58 +0200

    ds

--- main.c
+++ main.c
@@ -16,23 +16,6 @@ int main(int ac, char **av)
 		"add $3, %0"
 		: "=r" (fd)
 		: "r" (fd));
-
-	if (ac == 2)
-	{
-		fd = open(av[1], O_RDONLY);
-		if (fd == -1)
-		{
-			if (errno == EACCES)
-				exit(126);
-			if (errno == ENOENT)
-			{
-				_eputs(av[0]);
-				_eputs(": 0: Can't open ");
-				_eputs(av[1]);
-				_eputchar('\n');
-				_eputchar(BUF_FLUSH);
-				exit(127);
-			}
 			return (EXIT_FAILURE);
 		}
 		info->readfd = fd;
