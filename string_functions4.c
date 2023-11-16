00092507e4bf4609487537b1089b909aab650b3f
Deadeen <141264828+Deadeen@users.noreply.github.com>
2023-11-16 04:30:07 +0200

    func

--- string_functions4.c
+++ string_functions4.c
@@ -2,22 +2,22 @@
 
 /**
  * **strtow - splits a string into words. Repeat delimiters are ignored
- * @str: the input string
+ * @denstr: the input string
  * @d: the delimeter string
  * Return: a pointer to an array of strings, or NULL on failure
  */
 
-char **strtow(char *str, char *d)
+char **strtow(char *denstr, char *d)
 {
 	int i, j, k, m, numwords = 0;
 	char **s;
 
-	if (str == NULL || str[0] == 0)
+	if (denstr == NULL || denstr[0] == 0)
 		return (NULL);
 	if (!d)
 		d = " ";
-	for (i = 0; str[i] != '\0'; i++)
-		if (!is_delim(str[i], d) && (is_delim(str[i + 1], d) || !str[i + 1]))
+	for (i = 0; denstr[i] != '\0'; i++)
+		if (!is_delim(denstr[i], d) && (is_delim(denstr[i + 1], d) || !denstr[i + 1]))
 			numwords++;
 
 	if (numwords == 0)
@@ -27,10 +27,10 @@ char **strtow(char *str, char *d)
 		return (NULL);
 	for (i = 0, j = 0; j < numwords; j++)
 	{
-		while (is_delim(str[i], d))
+		while (is_delim(denstr[i], d))
 			i++;
 		k = 0;
-		while (!is_delim(str[i + k], d) && str[i + k])
+		while (!is_delim(denstr[i + k], d) && denstr[i + k])
 			k++;
 		s[j] = malloc((k + 1) * sizeof(char));
 		if (!s[j])
@@ -41,7 +41,7 @@ char **strtow(char *str, char *d)
 			return (NULL);
 		}
 		for (m = 0; m < k; m++)
-			s[j][m] = str[i++];
+			s[j][m] = denstr[i++];
 		s[j][m] = 0;
 	}
 	s[j] = NULL;
@@ -50,20 +50,20 @@ char **strtow(char *str, char *d)
 
 /**
  * **strtow2 - splits a string into words
- * @str: the input string
+ * @denstr: the input string
  * @d: the delimeter
  * Return: a pointer to an array of strings, or NULL on failure
  */
-char **strtow2(char *str, char d)
+char **strtow2(char *denstr, char d)
 {
 	int i, j, k, m, numwords = 0;
 	char **s;
 
-	if (str == NULL || str[0] == 0)
+	if (denstr == NULL || denstr[0] == 0)
 		return (NULL);
-	for (i = 0; str[i] != '\0'; i++)
-		if ((str[i] != d && str[i + 1] == d) ||
-		    (str[i] != d && !str[i + 1]) || str[i + 1] == d)
+	for (i = 0; denstr[i] != '\0'; i++)
+		if ((denstr[i] != d && denstr[i + 1] == d) ||
+		    (denstr[i] != d && !denstr[i + 1]) || denstr[i + 1] == d)
 			numwords++;
 	if (numwords == 0)
 		return (NULL);
@@ -72,10 +72,10 @@ char **strtow2(char *str, char d)
 		return (NULL);
 	for (i = 0, j = 0; j < numwords; j++)
 	{
-		while (str[i] == d && str[i] != d)
+		while (denstr[i] == d && denstr[i] != d)
 			i++;
 		k = 0;
-		while (str[i + k] != d && str[i + k] && str[i + k] != d)
+		while (denstr[i + k] != d && denstr[i + k] && denstr[i + k] != d)
 			k++;
 		s[j] = malloc((k + 1) * sizeof(char));
 		if (!s[j])
@@ -86,7 +86,7 @@ char **strtow2(char *str, char d)
 			return (NULL);
 		}
 		for (m = 0; m < k; m++)
-			s[j][m] = str[i++];
+			s[j][m] = denstr[i++];
 		s[j][m] = 0;
 	}
 	s[j] = NULL;
