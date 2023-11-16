70d5c400c7ba28a1070e66ac1b28446377ea228b
Deadeen <141264828+Deadeen@users.noreply.github.com>
2023-11-16 03:57:46 +0200

    func

--- env.c
+++ env.c
@@ -8,25 +8,25 @@
  */
 int _myenv(info_t *info)
 {
-	print_list_str(info->env);
+	print_list_str(info->theenv);
 	return (0);
 }
 
 /**
  * _getenv - gets the value of an myuniver variable
  * @info: Structure containing potential arguments. Used to maintain
- * @name: env var name
+ * @name: theenv var name
  *
  * Return: the value
  */
 char *_getenv(info_t *info, const char *name)
 {
-	list_t *node = info->env;
+	list_t *node = info->theenv;
 	char *p;
 
 	while (node)
 	{
-		p = starts_with(node->str, name);
+		p = starts_with(node->denstr, name);
 		if (p && *p)
 			return (p);
 		node = node->next;
@@ -43,12 +43,12 @@ char *_getenv(info_t *info, const char *name)
  */
 int _mysetenv(info_t *info)
 {
-	if (info->argc != 3)
+	if (info->beinsh != 3)
 	{
 		_eputs("Incorrect number of arguements\n");
 		return (1);
 	}
-	if (_setenv(info, info->argv[1], info->argv[2]))
+	if (_setenv(info, info->beins[1], info->beins[2]))
 		return (0);
 	return (1);
 }
@@ -63,19 +63,19 @@ int _myunsetenv(info_t *info)
 {
 	int i;
 
-	if (info->argc == 1)
+	if (info->beinsh == 1)
 	{
 		_eputs("Too few arguements.\n");
 		return (1);
 	}
-	for (i = 1; i <= info->argc; i++)
-		_unsetenv(info, info->argv[i]);
+	for (i = 1; i <= info->beinsh; i++)
+		_unsetenv(info, info->beins[i]);
 
 	return (0);
 }
 
 /**
- * populate_env_list - populates env linked list
+ * populate_env_list - populates theenv linked list
  * @info: Structure containing potential arguments. Used to maintain
  *          constant function prototype.
  * Return: Always 0
@@ -87,6 +87,6 @@ int populate_env_list(info_t *info)
 
 	for (i = 0; myuniver[i]; i++)
 		add_node_end(&node, myuniver[i], 0);
-	info->env = node;
+	info->theenv = node;
 	return (0);
 }
