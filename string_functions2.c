6207c19dedffe4a04f55216fe8a00830135ef948
Deadeen <141264828+Deadeen@users.noreply.github.com>
2023-11-16 02:12:03 +0200

    SIZE_OF_BUF_WRITE

--- string_functions2.c
+++ string_functions2.c
@@ -74,9 +74,9 @@ void _puts(char *str)
 int _putchar(char c)
 {
 	static int i;
-	static char buf[WRITE_BUF_SIZE];
+	static char buf[SIZE_OF_BUF_WRITE];
 
-	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	if (c == BUF_FLUSH || i >= SIZE_OF_BUF_WRITE)
 	{
 		write(1, buf, i);
 		i = 0;
